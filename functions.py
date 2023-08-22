c686be64123bfc34830a49d7a764a7fbab6799c9
johnnybro360 <abest82@gmail.com>
2023-08-22 11:58:49 +0800

    "restore function subtract; fixes #2"

--- functions.py
+++ functions.py
@@ -8,6 +8,8 @@ def multiply(a, b):
     return a * b
 
 def convert_fahrenheit_to_celsius(fahrenheit):
-    return multiply(subtract(fahrenheit, 32), 5 / 9)
+    if fahrenheit >= 0:
+        return multiply(subtract(fahrenheit, 32), 5 / 9)
+    raise AssertionError("something's wrong")
     # <-- Fix this in step 7
 
